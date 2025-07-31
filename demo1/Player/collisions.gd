f9fcc0010a049fc09f15b1baa3584a7641423b52
MRAbhighna <bbsf.personal@gmail.com>
2025-07-31 11:30:42 +0530

    Added Ship and Collisions

---
+++ demo1/Player/collisions.gd
@@ -0,0 +1,14 @@
+class_name GetCollision
+extends Node2D
+
+@export var Ship: CollisionShape2D
+@export var RayCast: RayCast2D
+
+func GetCollision(ShipCollision: bool, RayCastCollision: bool):
+	assert(ShipCollision == true and RayCastCollision == true, "ERROR: Can't return both CollisionShapes at the same time!")
+	assert(ShipCollision == false and RayCastCollision == false, "ERROR: Please pick a CollisionShape!") #Checking for Null values for both bools
+	
+	if RayCastCollision:
+		return RayCast
+	else:
+		return Ship
